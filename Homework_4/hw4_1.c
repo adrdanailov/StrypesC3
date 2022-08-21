ebfbd1c6f682d490a3276c9cf58b34cd2f9858cb
Adrian Danailov <adr.danailov@gmail.com>
2022-08-21 19:27:03 +0300

    Homework 4 task 1 - return -1 empty, 0 OK, 1 all equals
    it had the dual function to return max

--- Homework_4/hw4_1.c
+++ Homework_4/hw4_1.c
@@ -12,26 +12,35 @@ int array[]={ 10, 1, 5, -1, 113, 112 };
 //int array[] = {};
 size_t arr_size = sizeof(array)/sizeof(array[0]);
 int secondMax = 0;
-secondMaxfunc(array, arr_size, &secondMax);
-printf("\nSecondMAX = %d ", secondMax);
 
-putchar('\n');
+int ret = secondMaxfunc(array, arr_size, &secondMax);
+
+if (!ret){
+  printf("\nSecondMAX = %d \n", secondMax);
+}else if(ret>0)
+{
+  printf("\nThe array has only one entry: %d", *array);
+}else
+{
+  printf("\nThe array is empty!");
+}
+
 
 return 0;
 
 }
 
-int secondMaxfunc(const int* arr, size_t n, int* secondMax) {
+int secondMaxfunc(const int* arr, size_t n, int* secondMax) { //return -1 empty, 0 OK, 1 all equals
   int max = arr[0];
   int counter = 0;
   if(n <= 0)
   {
-    printf("\nThe array is empty!");
-    return 0;
+    //printf("\nThe array is empty!");
+    return -1;
   }else if (n == 1)
   {
-    printf("\nThe array has only one entry: %d ", *arr);
-    return max;
+    //printf("\nThe array has only one entry: %d ", *arr);
+    return 1;
   }else
   {
     for (int i = 1; i < n; i++)
@@ -48,9 +57,9 @@ int secondMaxfunc(const int* arr, size_t n, int* secondMax) {
       if(arr[n-1] != max && arr[n-1] > *secondMax)
         *secondMax = arr[n-1];
       if(counter == n-1)
-        printf("\nAll values are equal!");
+        //printf("\nAll values are equal!");
       //printf("\n%d %d", max, *secondMax);
-      return max;
+      return 0;
   }
 
 }
