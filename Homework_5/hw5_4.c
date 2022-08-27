522c61509ad2167bdbde5f9cf5ca8de43b83e3b7
Adrian Danailov <adr.danailov@gmail.com>
2022-08-28 01:57:44 +0300

    Hl e

---
+++ Homework_5/hw5_4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+{
+    int number;
+
+    float temp, sqrt;
+
+    printf("Provide the number: \n");
+
+    scanf("%d", &number);
+
+    sqrt = number / 2;
+    temp = 0;
+    while(sqrt != temp){
+        temp = sqrt;
+        sqrt = ( number/temp + temp) / 2;
+        printf("\ntemp=%f sqrt=%f", temp, sqrt);
+    }
+
+    printf("\nThe square root of '%d' is '%f'", number, sqrt);
+}
