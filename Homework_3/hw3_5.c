269d45596fc80e69a4201654e01b191b0e79bcc7
Adrian Danailov <adr.danailov@gmail.com>
2022-09-08 15:42:53 +0300

    EDIT: func prototypes, func comp. 1ull for 64 bit

--- Homework_3/hw3_5.c
+++ Homework_3/hw3_5.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+
+#define CHECKBIT(mask,bit) !!(mask & (1ull << (bit)))
+
+int onesCount(uint64_t);
+int onesCount_built_in(uint64_t);
+int onesCount_check_bit(uint64_t);
+
+
+int main (void)
+{
+	uint64_t mask;
+
+	printf("\nEnter a INT number: ");
+	scanf("%lu", &mask);
+	printf("\n%lu\n", mask);
+	printf("\nNumber of 1s is: %d - %d - %d (control value)", onesCount(mask), onesCount_check_bit(mask), onesCount_built_in(mask));
+
+	putchar('\n');
+
+	return EXIT_SUCCESS;
+}
 
 int onesCount(uint64_t x)
 {
     int count;
-    for (count=0; x; count++)
-        x &= x - 1;
+    for (count = 0; x; count++)
+        x &= x - 1ull;
     return count;
 }
 
-int onesCount_build_in(uint64_t mask)
+int onesCount_check_bit(uint64_t x)
 {
-	return  __builtin_popcountll(mask);
+    int count = 0;
+    for (int i = 0; i<64; i++)
+        if (CHECKBIT(x , count))
+            count++;
+    return count;
 }
 
-
-int main (){
-
-uint64_t mask;
-
-printf("\nEnter a INT number: ");
-scanf("%lu", &mask);
-printf("\n%lu\n", mask);
-printf("\nNumber of 1s is: %u", onesCount(mask));
-
-putchar('\n');
-
-return 0;
-
+int onesCount_built_in(uint64_t mask)
+{
+	return  __builtin_popcountll(mask);
 }
