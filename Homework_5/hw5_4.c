5d66c495f9e72786651abf1c676ee3848c73ac92
Adrian Danailov <adr.danailov@gmail.com>
2022-09-09 19:09:36 +0300

    EDIT:  SQRT algo based on the Babylonian method
    https://en.wikipedia.org/wiki/Methods_of_computing_square_roots

--- Homework_5/hw5_4.c
+++ Homework_5/hw5_4.c
@@ -1,22 +1,16 @@
 #include<stdio.h>
 
-void main()
+void main(void)
 {
-    int number;
-
-    float temp, sqrt;
-
-    printf("Provide the number: \n");
-
-    scanf("%d", &number);
-
+    double number;
+    double temp, sqrt;
+    printf("Provide the number: ");
+    scanf("%lf", &number);
     sqrt = number / 2;
     temp = 0;
     while(sqrt != temp){
         temp = sqrt;
-        sqrt = ( number/temp + temp) / 2;
-        printf("\ntemp=%f sqrt=%f", temp, sqrt);
+        sqrt = (temp + number/temp ) / 2;
     }
-
-    printf("\nThe square root of '%d' is '%f'", number, sqrt);
+    printf("\nThe square root of %0.4lf is %0.4lf", number, sqrt);
 }
