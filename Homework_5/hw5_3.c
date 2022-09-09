c475318a909b158191126929e2a202e8fc5e8829
Adrian Danailov <adr.danailov@gmail.com>
2022-09-09 18:53:10 +0300

    EDIT: cleaned debug 'printf' leftover, % - gives more random digits

--- Homework_5/hw5_3.c
+++ Homework_5/hw5_3.c
@@ -1,8 +1,7 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-void printarr (double*, int,size_t);
+void printarr(double*, int,size_t);
 int setRandom(double*, int, size_t, int);
 
 int main (void){
@@ -18,19 +17,15 @@ int main (void){
     setRandom(num, 0, n, 0);
     printarr(num, 0, n);
 
-
     //m
     printf("\nEnter M:");
     scanf("%d", &m);
     num = realloc(num, (n+m)*sizeof(double));
-   // printf("\n%d",n+m);
     if (NULL == num){
         return EXIT_FAILURE;
     }
-   // printf("\n%lu", sizeof(num)/sizeof(num[0]));
     setRandom(num, n, m, 1);
     printarr(num, n, m);
-
 
     //p
     printf("\nEnter P:");
@@ -46,28 +41,23 @@ int main (void){
     printarr(num, 0, n+m+p);
     free(num);
     return EXIT_SUCCESS;
-
 }
-
 
 void printarr (double* num, int start, size_t size)
 {
         printf("\n");
         for (int i = 0; i < size; ++i)
             printf("%d: %.5f\n", i+1, num[start+i]);
-        printf("\n");
-    }
+}
 
 int setRandom(double* num, int start, size_t size, int range)
 {
 
    srand( (unsigned)time( NULL ) );
    for (int i = 0; i < size; ++i) {
-      num[start+i] = rand() % 1000000;
+      num[start+i] = rand() % 1000000; // gives more 'random' digits
       num[start+i] = (num[start+i] / 1000000) + range;
-      //printf("%.5f\n", r[i]);
    }
 
    return EXIT_SUCCESS;
 }
-
