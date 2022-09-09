74ec297911a558ac5e9040e07b037a5d6adbba95
Adrian Danailov <adr.danailov@gmail.com>
2022-09-09 18:43:22 +0300

    EDIT: removed timer, improved console output

--- Homework_5/hw5_1.c
+++ Homework_5/hw5_1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <time.h>
+#include <stdlib.h>
 
 #define MAX_INT_X32 2147483647
 
@@ -15,10 +15,9 @@
 #define INVERSEBIT_X32(mask,bit) (mask ^= (1 << (bit)))
 #define CHECKBIT_X32(mask,bit) !!(mask & (1 << (bit)))
 
-#define CHK_X64(a) ((a) > MAX_INT_X32 ? 1 : 0)
+#define CHK_X64(a) ((a) > MAX_INT_X32 ? 1 : 0) //checks if there is a need to go past 32-nd bit
 
 #define FORIBITS(start, stop) for(int i = (stop); i >= (start); i--)
-
 
 #define SWAP_TOO_COMPLICATED(a,b) if(CHK_X64(a) | CHK_X64(b)) \
                         FORIBITS(0, 63) { ((CHECKBIT((a), i)) ^ (CHECKBIT((b), i))) ? \
@@ -27,26 +26,22 @@
                         FORIBITS(0, 31) { ((CHECKBIT_X32((a), i)) ^ (CHECKBIT_X32((b), i))) ? \
                             (INVERSEBIT_X32((a) ,i) && INVERSEBIT_X32((b) ,i)) : 0 ; }
 
-#define PRINTBITS(a) FORIBITS(0, (CHK_X64(a) ? 63 : 31))printf("%d", CHECKBIT(a, i));
+#define PRINTBITS(a) FORIBITS(0, (CHK_X64(a) ? 63 : 31))printf("%d", CHECKBIT(a, i))
 
-#define SWAP(a,b) (a)^=(b);(b)^=(a);(a)^=(b);
+#define SWAP(a,b) (a)^=(b);(b)^=(a);(a)^=(b)
 
 int main(){
 
     uint64_t a, b, c;
-    int ch,ii=10000001;
+    int ch;
     uint64_t AA,BB;
-    clock_t t;
-    double time_taken;
 
     do {
         printf("\n***Choose option***");
         printf("\n1: MIN/MAX");
         printf("\n2: SWAP");
         printf("\n3: EXIT\n\noption:");
-
         scanf("%d", &ch);
-
         switch (ch)
         {
             case 1:
@@ -66,41 +61,35 @@ int main(){
 
                 printf("\nEnter B: ");
                 scanf("%lu", &BB);
+                printf("\nORIGINAL\n[decimal]: ");
+                printf(" A=%lu B=%lu\n", AA, BB);
+                printf("\n[binary]: \n");
+                PRINTBITS(AA);
+                putchar('\n');
+                PRINTBITS(BB);
+                putchar('\n');
+                //swap unswap swap
+                SWAP(AA,BB);
+                SWAP_TOO_COMPLICATED(AA, BB);
+                SWAP(AA,BB);
+                //both work
+                printf("\nSWAPPED\n[decimal]: ");
+                printf(" A=%lu B=%lu\n", AA, BB);
+                printf("\n[binary]: \n");
+                PRINTBITS(AA);
+                putchar('\n');
+                PRINTBITS(BB);
+                putchar('\n');
+                break;
 
-                printf("\nA=%lu B=%lu\n", AA, BB);
-
-                printf("\nORIGINAL[binary]: \n");
-                    PRINTBITS(AA)
-                    putchar('\n');
-                    PRINTBITS(BB)
-                    putchar('\n');
-t = clock();AA*=1ull;
-while(ii){
-                SWAP(AA,BB)
-                //SWAP_TOO_COMPLICATED(AA, BB);
-ii--;
-}
-t = clock() - t;
-
-                printf("\nSWAPED[binary]: \n");
-
-time_taken = ((double)t)/CLOCKS_PER_SEC;
-printf("SWAP(a, b) took %.7f seconds to execute \n", time_taken);
-//printf("SWAP_TOO_COMPLICATED(a, b) took %.7f seconds to execute \n", time_taken);
-
-                    PRINTBITS(AA)
-                    putchar('\n');
-                    PRINTBITS(BB)
-                    putchar('\n');
-                    break;
             case 3:
                 return 0;
                 break;
+
             default:
                 printf("\n\tNo valid entry! \n");
                 break;
         }
     }while(1);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
