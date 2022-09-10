2d293c3f22ef9f9a84c9e197f8b54e3f6be3713f
Adrian Danailov <adr.danailov@gmail.com>
2022-09-10 16:03:59 +0300

    zad2.c

---
+++ FINAL_TEST/zad2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+unsigned sumArrayDigits(const int* arr, size_t n);
+
+int main(void)
+{
+    int array[]={ 12, 34, 5, 70  };
+    size_t arr_size = sizeof(array)/sizeof(array[0]);
+    printf("\nThe array digital root sum is = %u\n", sumArrayDigits(array, arr_size));
+    return 0;
+}
+
+unsigned sumArrayDigits(const int* arr, size_t n)
+{
+    unsigned int sum = 0;
+    for (int i = 0; i < n; ++i){
+        if ( arr[i] < 10 )
+            sum += arr[i];
+        else
+        {
+            unsigned temp = arr[i];
+            while (temp != 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+        }
+    }
+return sum;
+}
